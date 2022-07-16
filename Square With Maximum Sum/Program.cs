d30692b662b7ce2e8327c2a6a016d4e0a28a23cf
Dimo Todorov <baal98@gmail.com>
2022-07-16 19:25:46 +0300

    Add files via upload

---
+++ Square With Maximum Sum/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[,] matrix = new int[input[0], input[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] arr = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = arr[col];
+                }
+            }
+
+            int maxSum = int.MinValue;
+            int rowPosition = 0;
+            int colPosition = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    int sum = matrix[row, col] + matrix[row, col + 1] + matrix[row + 1, col] + matrix[row + 1, col + 1];
+
+                    if (sum > maxSum)
+                    {
+                        rowPosition = row;
+                        colPosition = col;
+                        maxSum = sum;
+                    }
+                }
+            }
+
+            for (int row = rowPosition; row < rowPosition + 2; row++)
+            {
+                for (int col = colPosition; col < colPosition + 2; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(maxSum);
+        }
+    }
+}
