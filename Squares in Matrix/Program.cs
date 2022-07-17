351ee4ddd371769ae2c63aa3e2ffe9036bfc7439
Dimo Todorov <baal98@gmail.com>
2022-07-17 19:46:10 +0300

    Add files via upload

---
+++ Squares in Matrix/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Squares_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            char[,] matrix = new char[inputs[0], inputs[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] chars = Console.ReadLine().Split();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = Convert.ToChar(chars[col]);
+                }
+            }
+
+            int counter = 0;
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1]
+                        && matrix[row, col] == matrix[row + 1, col]
+                        && matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        counter++;
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
