687a358595b68e884c5e8a90e58bf62eb4f60748
Dimo Todorov <baal98@gmail.com>
2022-07-24 15:55:43 +0300

    Add files via upload

---
+++ Predicate for Names/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Predicate_for_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nameLength = int.Parse(Console.ReadLine());
+
+            var names = Console.ReadLine().Split();
+
+            Func<string, bool> func = x => x.Length <= nameLength;
+
+            Console.WriteLine(string.Join("\n", names.Where(func)));
+        }
+    }
+}
