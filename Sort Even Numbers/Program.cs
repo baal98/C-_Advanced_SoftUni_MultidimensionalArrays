78665a9e4e06f66623414bbf4e382656042a1228
Dimo Todorov <baal98@gmail.com>
2022-07-25 06:22:33 +0300

    Add files via upload

---
+++ Sort Even Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(", ").Select(int.Parse).Where(x => x % 2 == 0).OrderBy(x => x);
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
