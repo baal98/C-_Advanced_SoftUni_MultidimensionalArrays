f7f92e36852faa8269c0569887c2c43909fe5fad
Dimo Todorov <baal98@gmail.com>
2022-07-25 06:21:41 +0300

    Add files via upload

---
+++ Reverse And Exclude/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace Reverse_And_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = Console.ReadLine().Split(' ').Select(int.Parse).Reverse();
+            int divisor = int.Parse(Console.ReadLine());
+
+            Predicate<int> predicate = x => x % divisor != 0;
+
+            inputs = inputs.Where(x => predicate(x));
+
+            Console.WriteLine(String.Join(" ", inputs));
+
+            //Here is using Func<int, bool>
+            //Func<int, bool> predicate1 = x => x % divisor != 0;
+            //inputs = inputs.Where(predicate1);
+            //Console.WriteLine(String.Join(" ", inputs));
+        }
+    }
+}
